a4878812e1ae80bf57be1fb761e6b5fc6c0d322a
DoczzzMega <roma.gunko87@gmail.com>
2025-06-01 00:53:21 +0500

    add port in sail docker-compose.yml

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\LoadExcelController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', view: 'welcome');
+
 Route::middleware('auth.basic')->group(function () {
     Route::get('load-excel', [LoadExcelController::class, 'index']);
     Route::post('load-excel', [LoadExcelController::class, 'store'])->name('load-excel.store');
